72166ef2e8544fff8a1fb315a2e65729a9f703f3
BrooksPatton <brooks-patton@sbcglobal.net>
2014-08-19 16:34:07 -0600

    We need to send the twitch keys to heroku

---
+++ config/twitch-keys.js
@@ -0,0 +1,12 @@
+/**
+ * The Twitch.tv client and secret keys.
+ */
+var keys = {
+	clientId: '4napf8r7y5dhn2o6h7e676yil5dift4',
+	clientSecret: 'cbh6cea2nsunwiklmq6luc98xh3nq6t'
+};
+
+/**
+ * Make the twitch-keys available to other node files
+ */
+ module.exports = keys;
